152e0faefed229fa03875837e217e66c0101fd0b
victor.g <victor.vinicius.g@hotmail.com>
2025-06-23 01:23:05 -0300

    Euler-1: Problem #1 solved. Minor fix in text.

--- problem_1/multiples_3_5.c
+++ problem_1/multiples_3_5.c
@@ -36,7 +36,7 @@ void get_sum_of_three_and_five(int *n){
 
 void main(){
     // gets the cumulative sum up to 1000, included.
-    // The answer to Euler's Project problem #1
+    // The answer to Project Euler's problem #1
     // will be the last output of this loop.
     for(int i = 10; i <= 1000; i++){
         get_sum_of_three_and_five(&i);
